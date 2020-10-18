fc0e2f5c885246ce97a5bab9236b67b58aee0b63
adw63 <adw63@cs-ssh.argo.uwf.edu>
2020-10-18 17:32:04 -0500

    uhhh basically done almost in one sitting haha

--- Mt-collatz.cpp
+++ Mt-collatz.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <thread>
 #include <pthread.h>
+#include <mutex>
 
-void rangeIsEven(int& n);
-void rangeIsOdd(int& n);
-void ex(int x);
+void rangeIsEven(int n);
+void rangeIsOdd(int n);
+//void ex(int x);
 int range = 2;
 int numOfThreads = 1;
-int p = 22;
+int currentThread = 0;
+//int p = 22;
 
 
 int main(int argc, char* argv[])
@@ -17,44 +19,53 @@ int main(int argc, char* argv[])
 	
 	range = std::atoi(argv[1]);
 	numOfThreads = std::atoi(argv[2]);
+	std::mutex mtx;
 
-	std::thread threads[3];
-	threads[0] = std::thread(ex, 1);
-	threads[0].join();
-	std::cout << p << std::endl;
+	std::thread threads[numOfThreads];
+	//threads[0] = std::thread(ex, range);
+	//threads[0].join();
+	//std::cout << range << std::endl;
 	//std::cout << range << " " << numOfThreads << std::endl;
-	/*
+	
 	while (range != 1)
 	{
+		if (currentThread == numOfThreads)
+			currentThread = 0;
+
+		mtx.lock();
 		if (range % 2 == 0)
 		{
-			//threads[0] = std::thread(rangeIsEven, 3);
-			rangeIsEven(range);
+			
+			threads[currentThread] = std::thread(rangeIsEven, range);
 		}
 		else
 		{
-			rangeIsOdd(range);
-
+			threads[currentThread] = std::thread(rangeIsOdd, range);
 		}
+		mtx.unlock();
+
 		std::cout << range << std::endl;
+		threads[currentThread].join();
+		currentThread++;
 	}
-	*/
+	
 	
 }
 
 
-void rangeIsEven(int& n)
+void rangeIsEven(int n)
 {
-	n = n / 2;
+	range = n / 2;
 }
 
-void rangeIsOdd(int& n)
+void rangeIsOdd(int n)
 {
-	n = (3 * n) + 1;
+	range = (3 * n) + 1;
 }
 
-
+/*
 void ex(int x)
 {
-	p = x;
+	range = x / 2;
 }
+*/
