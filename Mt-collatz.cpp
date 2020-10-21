d83431513585161d305afc41d8ed77304321f0a1
adw63 <adw63@cs-ssh.argo.uwf.edu>
2020-10-21 17:20:34 -0500

    adding multithreading

--- Mt-collatz.cpp
+++ Mt-collatz.cpp
@@ -5,74 +5,78 @@
 #include <vector>
 #include <queue>
 
-void rangeIsEven(int n);
-void rangeIsOdd(int n);
+void rangeIsEven(int& n);
+void rangeIsOdd(int& n);
+void computeStoppingTime(int num);
 
-int ZA_WARUDO = 0;
+int stopValue = 0;
 int numOfThreads = 1;
 int currentThread = 0;
 int counter = 0;
-std::vector<int> histogram;
-
+int histogram[550];
 
 int main(int argc, char* argv[])
 {
 	
-	int ceiling = 0;
-	std::queue<int> holder;
+	int range = 0;
+	std::mutex mtx;
+	std::thread threads[numOfThreads];
+
 	if (argv[1] != NULL )
 	{
-		ceiling = std::atoi(argv[1]);
+		range = std::atoi(argv[1]);
 		numOfThreads = std::atoi(argv[2]);
 	}
 
-	ZA_WARUDO = ceiling;
-	std::mutex mtx;
-
-	std::thread threads[numOfThreads];
-	//threads[0] = std::thread(ex, range);
-	//threads[0].join();
-	//std::cout << range << std::endl;
-	//std::cout << range << " " << numOfThreads << std::endl;
 	if (argv[1] != NULL)
 	{
-		while (ZA_WARUDO != 1)
+		for (int i = 2; i <= range; i++)
 		{
-			if (currentThread == numOfThreads)
+			if (currentThread == numOfThreads) 
 				currentThread = 0;
-
+			
 			mtx.lock();
-			if (ZA_WARUDO % 2 == 0)
-			{
-				threads[currentThread] = std::thread(rangeIsEven, ZA_WARUDO);
-			}
-			else
-			{
-				threads[currentThread] = std::thread(rangeIsOdd, ZA_WARUDO);
-			}
+			stopValue = 0;
+			threads[currentThread] = std::thread(computeStoppingTime, i);
 			mtx.unlock();
 
-			
-			std::cout << ZA_WARUDO << std::endl;
-			holder.push(ZA_WARUDO);
+			//std::cout << i << std::endl;
 			threads[currentThread].join();
-			currentThread++;
-			counter++;
 		}
-		//resize to the highest value in the holder queue for the array to resize it later TODO 
+	}
+}
 
-		//std::cout << counter << "\n";
+void computeStoppingTime(int num)
+{
+
+	while (num > 1)
+	{
+		if (num % 2 == 0)
+		{
+			rangeIsEven(num);
+		}
+		else
+		{
+			rangeIsOdd(num);
+		}
+		stopValue++;
+	}
+
+	if (stopValue > 550)
+	{
+		//std::cout << stopValue << std::endl;
+
 	}
 }
 
 
-void rangeIsEven(int n)
+void rangeIsEven(int& n)
 {
-	ZA_WARUDO = n / 2;
+	n = n / 2;
 }
 
-void rangeIsOdd(int n)
+void rangeIsOdd(int& n)
 {
-	ZA_WARUDO = (3 * n) + 1;
+	n = (3 * n) + 1;
 }
 
